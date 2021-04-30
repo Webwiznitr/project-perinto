9660e4a957acc68ecd26627c2c2d702986bfdcba
sumana basu <sumana.basu2001@gmail.com>
2021-04-30 23:48:08 +0530

    table added

--- src/pages/Beds.jsx
+++ src/pages/Beds.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Table from '../components/table/table';
 
 function Beds() {
     return (
         <div className="App">
-            <h1>Coming Soon</h1>
+            <h1>Coming Soon. This is dummy data</h1>
+            <Table/>
         </div>
     )
 }
